f5c3bbc7b3b29a6e0c86bd2349b337e4c28a26c3
RecursionHs <sknoendideal@163.com>
2020-02-06 18:37:51 +0800

    java8

--- myJvm/lamda/src/main/java/com/hs/defaultRealize/StartGame.java
+++ myJvm/lamda/src/main/java/com/hs/defaultRealize/StartGame.java
@@ -1,5 +1,9 @@
 package com.hs.defaultRealize;
 
+import org.junit.Test;
+
+import static java.lang.Math.sqrt;
+
 public class StartGame {
 
     public static void main(String[] args) {
@@ -8,5 +12,22 @@ public class StartGame {
         double calculate1 = calculate.calculate(4);
         System.out.println(sqrt);
         System.out.println(calculate1);
+    }
+
+    @Test
+    public void Test1(){
+        Formula formula = new Formula() {
+            @Override
+            public double calculate(int a) {
+                return sqrt(a);
+            }
+        };
+        System.out.println(formula.calculate(6));
+    }
+
+    @Test
+    public void Test2(){
+        Formula formula = (a)-> sqrt(a);
+        System.out.println(formula.calculate(6));
     }
 }
---
+++ myJvm/lamda/src/main/java/com/hs/lamda/demo/Person.java
@@ -0,0 +1,13 @@
+package com.hs.lamda.demo;
+
+public class Person {
+    String firstName;
+    String lastName;
+
+    public Person(){}
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
