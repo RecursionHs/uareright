a001db1e6c44a9f9bad141b4af73e80d4c6a4870
RecursionHs <sknoendideal@163.com>
2020-05-26 23:51:47 +0800

    the class study

---
+++ myJvm/jvmClass/src/main/java/com/hs/TestClass.java
@@ -0,0 +1,18 @@
+package com.hs;
+
+/**
+ * @author : sk
+ * @version V1.0
+ * @Project: myJvm
+ * @Package com.hs
+ * @Description: TODO
+ * @date Date : 2020年05月26日 下午11:09
+ */
+public class TestClass {
+
+    private int m;
+
+    public int inc(){
+        return m + 1;
+    }
+}
