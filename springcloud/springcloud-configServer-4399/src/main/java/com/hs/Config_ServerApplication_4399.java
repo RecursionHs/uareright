b36bfeb70753380817a3a35295d8a93376f0ef4b
RecursionHs <sknoendideal@163.com>
2020-07-14 23:27:52 +0800

    zuul and springcloud config

---
+++ springcloud/springcloud-configServer-4399/src/main/java/com/hs/Config_ServerApplication_4399.java
@@ -0,0 +1,14 @@
+package com.hs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Config_ServerApplication_4399 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Config_ServerApplication_4399.class,args);
+    }
+}
